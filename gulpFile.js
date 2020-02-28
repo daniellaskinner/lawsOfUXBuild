f60826b8a9c23443d9849b9397bee61da87b3201
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-28 16:11:43 +0000

    added navbar and hamburger menu icon

--- gulpFile.js
+++ gulpFile.js
@@ -1,3 +1,4 @@
+var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 function sassCompile(cb) {
@@ -5,8 +6,6 @@ function sassCompile(cb) {
         .pipe(sass()) // Converts Sass to CSS with gulp-sass
         .pipe(gulp.dest('app/css'));
     cb();
-.pipe(plugins.livereload()); //add live reload of page
-.pipe(gulp.dest('build')).on('error', gutil.log)
 }
 
 function watch() {
